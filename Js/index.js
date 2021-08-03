513f1d2160154f36ae9ad4021e65fbdc850b686d
Farid Amimri <farid.amimri@gmail.com>
2021-08-03 12:21:29 +0200

    photos updated

--- Js/index.js
+++ Js/index.js
@@ -5,19 +5,19 @@ function displayPhotographer(name, id, city, country, tags, tagline, price, port
     const link = document.createElement("a");
     link.href = "photograph.html";
     const image = document.createElement("img");
-    image.src = "../Photos/Photographers ID Photos/" + portrait;
+    image.src = "../Photos/PhotographersIDPhotos/" + portrait;
     link.append(image)
     container.append(link)
-    const profil = document.createElement("h2");
-    profil.textContent = name;
-    link.append(profil)
+    const title = document.createElement("h2");
+    title.textContent = name;
+    link.append(title)
     container.append(link)
 
     const article = document.createElement("div");
     article.className = "information";
     const location = document.createElement("p");
     location.className = "city";
-    location.textContent = city;
+    location.textContent = city + ", " + country;
     article.append(location)
     container.append(article)
     const biography = document.createElement("p");
@@ -30,8 +30,22 @@ function displayPhotographer(name, id, city, country, tags, tagline, price, port
     article.append(money)
     container.append(article)
 
+    const nav = document.createElement("nav");
+    const ul = document.createElement("ul");
 
+    for (let i = 0; i < tags.length; i++) {
+        
+        const li = document.createElement("li");
+        const linka = document.createElement("a");
+        linka.innerHTML = tags[i];
+        li.append(linka)
+        ul.append(li)
+    }
 
-
+    nav.append(ul)
+    container.append(nav)
+    document.querySelector('main').append(container);
 } 
-displayPhotographer()
+displayPhotographer("Mimi Keel", 243, "London", "FR", ["portrait", "events", "travel", "bullshit"], "Voir le beau dans le quotidien", 500, "MimiKeel.jpg");
+
+
